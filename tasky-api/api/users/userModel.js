0d2bfb1f370dab6f2bea5bc6ea18bbd0e217e317
michaelogunn <mogunnaike96@gmail.com>
2025-11-29 17:32:55 +0000

    Authentication lab - exercise completed

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -4,17 +4,25 @@ import bcrypt from 'bcrypt';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  username: { type: String, unique: true, required: true },
+  password: {
+    type: String, required: true, validate: {
+      validator: (password) => {
+        return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password);
+      }, message: 'Password must contain at least 8 characters, including uppercase letters, lowercase letters, numbers, and special characters.'
+    }
+  },
+  
+
 });
-UserSchema.methods.comparePassword = async function (passw) { 
-    return await bcrypt.compare(passw, this.password); 
+UserSchema.methods.comparePassword = async function (passw) {
+  return await bcrypt.compare(passw, this.password);
 };
 UserSchema.statics.findByUserName = function (username) {
   return this.findOne({ username: username });
 };
 
-UserSchema.pre('save', async function(next) {
+UserSchema.pre('save', async function (next) {
   const saltRounds = 10; // You can adjust the number of salt rounds
   //const user = this;
   if (this.isModified('password') || this.isNew) {
@@ -22,12 +30,12 @@ UserSchema.pre('save', async function(next) {
       const hash = await bcrypt.hash(this.password, saltRounds);
       this.password = hash;
       next();
-  } catch (error) {
-     next(error);
-  }
+    } catch (error) {
+      next(error);
+    }
 
   } else {
-      next();
+    next();
   }
 });
 
